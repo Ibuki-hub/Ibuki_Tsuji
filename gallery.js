6407bb31afdcb44fbe4f6b045cfbaba866a91ec5
Ibuki-hub <145672981+Ibuki-hub@users.noreply.github.com>
2023-10-04 09:05:11 +0900

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,6 +1,6 @@
 function pw(){
   PRO = prompt("パスワードを入力して「ok」ボタンを押してください。","");
-  const url = '/drive.google.com/drive/folders/' + 'PRO'
+  const url = 'https://drive.google.com/drive/folders/' + 'PRO'
   if(!(PRO=="" || PRO==null)){location.href='url'}
   else{alert("なにも入力されていないか、[Cancel]ボタンが押されました。")}
 }
